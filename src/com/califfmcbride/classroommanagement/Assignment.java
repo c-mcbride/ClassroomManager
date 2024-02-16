3c741d67719568c8156342c235d287a6ac20ab27
c-mcbride <califfmcbride@gmail.com>
2024-02-15 19:22:00 -0600

    Add assingment class

--- src/com/califfmcbride/classroommanagement/Assignment.java
+++ src/com/califfmcbride/classroommanagement/Assignment.java
@@ -1,5 +1,25 @@
 package com.califfmcbride.classroommanagement;
+import java.util.Date;
 
 public class Assignment {
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Course course;
+    private int assignmentId;
+
+    public Assignment(String title, String description, Date dueDate, Course course, int assignmentId){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.course = course;
+        this.assignmentId = assignmentId;
+    }
+
     
+
+
+
+
+
 }
