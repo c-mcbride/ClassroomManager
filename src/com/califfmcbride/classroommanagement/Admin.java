693aa666c6cd12654f0d173b4b5061aee2879f71
c-mcbride <califfmcbride@gmail.com>
2024-02-16 22:38:26 -0600

    Add admin class and drive code to test teacher list

---
+++ src/com/califfmcbride/classroommanagement/Admin.java
@@ -0,0 +1,46 @@
+package com.califfmcbride.classroommanagement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Admin {
+    private String name;
+    private int adminId;
+    private List<Teacher> teachers;
+
+    public Admin(String name, int adminId){
+        this.name = name;
+        this.adminId = adminId;
+        this.teachers = new ArrayList<>();
+    }
+
+    //Getters and Setters;
+    public String getName(){ return this.name; }
+    public void setName(String name){ this.name = name; }
+    public int getAdminId(){ return this.adminId; }
+    public void setAdminId(int adminId){ this.adminId = adminId;}
+    public List<Teacher> getTeachers(){ return this.teachers; }
+
+    //Methods
+    //Add teacher to school. Will need to gather this info in the main method
+    public void addTeacher(Teacher teacher){
+        teachers.add(teacher);
+    }
+
+    //Assign course to teacher. Construct course in main method
+    public void addCourseToTeacher(Teacher teacher, Course course){
+        teacher.addCourse(course);
+    }
+
+    //Add students to course
+    public void addStudentToCourse(Student student, Course course){
+        course.addStudent(student); //Enroll student in course
+        student.addCourse(course); //Add to students schedule
+    }
+
+    public void printTeacherList(){
+        for(Teacher teacher : teachers){
+            System.out.println("Teacher ID: " + teacher.getTeacherId() + ", Name: " + teacher.getName() + ", Email: " + teacher.getEmail());
+        }
+    }
+
+}
--- src/com/califfmcbride/classroommanagement/App.java
+++ src/com/califfmcbride/classroommanagement/App.java
@@ -1,35 +1,65 @@
 package com.califfmcbride.classroommanagement;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //Create a teacher
-       Teacher teacher = new Teacher(1, "Mr. Smith","Smith@utc.edu");
+        System.out.println("Welcome to ClassroomManager - Please Select and Option");
+        System.out.println("1 - Create an admin ");
+        System.out.println("0 - Exit");
 
-       //Create a course and assign it to a teacher
-       Course course = new Course("CS50", "Intro to computer science", "A beginner class on cs", teacher);
+        Scanner scanner = new Scanner(System.in);
+        int mainChoice = scanner.nextInt();
+        scanner.nextLine(); //Consume the remaining newline
 
-       //Add the course to the teachers list
-       course.addClassToTeacher(course);
+        switch(mainChoice){
+            case 1:
+              System.out.println("Please enter admin name: ");
+              String adminName = scanner.nextLine();
 
-       //Create a student and add them to the course
-       Student student = new Student("Alice", 1234);
+              System.out.println("Enter adminId: ");
+              int adminId = scanner.nextInt();
+              scanner.nextLine();
 
-       //Add student to course
-       course.addStudent(student);
+              //Construct admin object
+              Admin admin = new Admin(adminName, adminId);
+              boolean adminMenuActive = true;
 
-       //Create and assignment and add it to the course
-       Assignment assignment = new Assignment("Homework 1", "Solve problems 1 to 5", new Date(), course, 101);
-       teacher.addAssignment("CS50", assignment);
+              while(adminMenuActive){
+                System.out.println("Welcome " + adminName + " what would you like to do?");
+                System.out.println("1 - Add a teacher");
+                System.out.println("2 - Add a course");
+                System.out.println("3 - Exit");
 
-        Submission submission = new Submission(1, student, assignment, "My answers", LocalDateTime.now());
+                int adminMenuChoice = scanner.nextInt();
+                scanner.nextLine();
 
-        // Test outputs
-        System.out.println("Course: " + course.getName());
-        System.out.println("Teacher: " + course.getTeacher().getName());
-        System.out.println("Number of students in course: " + course.getStudents().size());
-        System.out.println("Number of assignments in course: " + course.getAssignments().size());
-        System.out.println("Submission content: " +submission.getContent());
+                switch(adminMenuChoice){
+                    case 1:
+                        System.out.println("Add a teacher");
+
+                        System.out.print("Enter teacher Id: ");
+                        int teacherId = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.print("Enter the teachers name: ");
+                        String teacherName = scanner.nextLine();
+
+                        System.out.print("Enter the teachers email: ");
+                        String teacherEmail = scanner.nextLine();
+
+                        Teacher teacher = new Teacher(teacherId, teacherName, teacherEmail);
+                        admin.addTeacher(teacher);
+                        break;
+                    case 2:
+                        admin.printTeacherList();
+                        break;
+                    case 3:
+                        adminMenuActive = false;
+                        break;
+                }
+            }
+        }
     }
 }
