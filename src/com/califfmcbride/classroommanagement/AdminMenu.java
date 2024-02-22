1c2a5d5e9a4f355e64f58c47787c357ee2d1bb6b
c-mcbride <califfmcbride@gmail.com>
2024-02-21 22:12:37 -0600

    Add adminmenu class to clean up main menu

---
+++ src/com/califfmcbride/classroommanagement/AdminMenu.java
@@ -0,0 +1,171 @@
+package com.califfmcbride.classroommanagement;
+import java.util.Scanner;
+
+public class AdminMenu {
+    private SchoolStaffService staffService;
+    private StudentDirectory studentDirectory;
+    private CourseService courseService;
+    private Admin admin;
+
+
+    public AdminMenu(SchoolStaffService staffService, StudentDirectory studentDirectory, CourseService courseService, Admin admin) {
+        this.staffService = staffService;
+        this.studentDirectory = studentDirectory;
+        this.courseService = courseService;
+        this.admin = admin;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu(){
+        boolean adminMenuActive = true;
+        while(adminMenuActive){
+            System.out.println("Welcome " + admin.getName() + " what would you like to do?");
+            System.out.println("1 - Add a teacher");
+            System.out.println("2 - Add a course");
+            System.out.println("3 - Add a student");
+            System.out.println("4 - Add a student to a course");
+            System.out.println("5 - View Students Enrolled in Course");
+            System.out.println("0 - Exit");
+
+            int adminMenuChoice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("-------------------------------------");
+
+            switch(adminMenuChoice){
+                case 1:
+                    System.out.println("Add a teacher");
+                    Boolean validInput = false;
+                    int teacherId = -1;
+
+                    //We need to make sure the user enters a number for teacher id
+                    while(!validInput){
+                        System.out.print("Enter teacher Id: ");
+                        if(scanner.hasNextInt()){
+                            teacherId = scanner.nextInt();
+                            scanner.nextLine();
+                            validInput = true;
+                        }
+                        else{
+                            System.out.println("Please enter a non-demical number for teacher input");
+                            scanner.nextLine();
+                        }
+                    }
+
+                    System.out.print("Enter the teachers name: ");
+                    String teacherName = scanner.nextLine();
+
+                    System.out.print("Enter the teachers email: ");
+                    String teacherEmail = scanner.nextLine();
+
+                    //We create a teacher object and add the teacher to the staffService instance
+                    Teacher teacher = new Teacher(teacherId, teacherName, teacherEmail);
+                    staffService.addTeacher(teacher);
+                    System.out.println("-------------------------------------");
+                    break;
+                case 2:
+                    System.out.println("Create a course");
+                    System.out.println("Please enter course ID");
+                    int courseId= scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.println("Enter the course name");
+                    String courseName = scanner.nextLine();
+
+                    System.out.println("Enter course description");
+                    String courseDescription = scanner.nextLine();
+                    System.out.println("-------------------------------------");
+
+                    staffService.listAllTeachers();
+                    System.out.println("Select a teacher to assign (enter teacher ID): ");
+                    int selectedTeacherId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    //Find the selected teacher
+                    Teacher selectedTeacher = staffService.findTeacherById(selectedTeacherId);
+
+                    //Create course object and add the teacher to it
+                    Course course = new Course(courseId, courseName, courseDescription, selectedTeacher);
+                    //Add course to CourseService
+                    courseService.addCourse(course);
+
+                    if(selectedTeacher != null){
+                        admin.addCourseToTeacher(selectedTeacher, course);
+                    }
+                    else{
+                        System.out.println("Teacher not found");
+                    }
+                    System.out.println("-------------------------------------");
+                    break;
+                case 3:
+                    System.out.println("Add a student");
+                    validInput = false;
+                    int studentId = -1;
+
+                    while(!validInput){
+                        System.out.print("Enter student Id");
+                        if(scanner.hasNextInt()){
+                            studentId = scanner.nextInt();
+                            scanner.nextLine();
+                            validInput = true;
+                        }
+                        else{
+                            System.out.println("Please enter a non-decimal number for student Id");
+                            scanner.nextLine();
+                        }
+                    }
+
+                    System.out.print("Enter the students name: ");
+                    String studentName = scanner.nextLine();
+
+                    System.out.print("Enter the students email: ");
+                    String studentEmail = scanner.nextLine();
+
+                    Student student = new Student(studentName, studentId, studentEmail);
+                    studentDirectory.addStudent(student);
+                    System.out.println("-------------------------------------");
+                    break;
+
+                case 4:
+                    System.out.println("Add a student to course");
+                    studentDirectory.listAllStudents();
+                    System.out.println("Select a student to assign (enter student ID): ");
+                    studentId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    //Find the selected student
+                    Student selectedStudent = studentDirectory.getStudentById(studentId);
+
+                    //Select course that you would like to add student to
+                    System.out.println("Please select a course to add the student to");
+                    courseService.listCourses();
+                    System.out.println("-------------------------------------");
+                    System.out.println("Enter course Id");
+                    courseId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    //Now retrieve the course object and add the student to it
+                    course = courseService.getCourseById(courseId);
+                    course.addStudent(selectedStudent);
+                    System.out.println("-------------------------------------");
+                    break;
+                case 5:
+                    System.out.println("View students enrolled in course");
+                    courseService.listCourses();
+
+                    //Locate the course object and print the students
+                    System.out.println("Enter the course Id for the course you want to see");
+                    courseId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    course = courseService.getCourseById(courseId);
+                    course.listStudentsInCourse();
+                    System.out.println("-------------------------------------");
+                    break;
+                case 0:
+                    adminMenuActive = false;
+                    break;
+            }
+        }
+    }
+}
--- src/com/califfmcbride/classroommanagement/App.java
+++ src/com/califfmcbride/classroommanagement/App.java
@@ -30,6 +30,7 @@ public class App {
               boolean validInput = false;
               int adminId = -1;
 
+              //Input validation to make sure they enter a number
               while(!validInput){
                 System.out.print("Enter admin Id: ");
                 if(scanner.hasNextInt()){
@@ -43,163 +44,16 @@ public class App {
                 }
               }
 
-              //Construct admin object
-              Admin admin = new Admin(adminName, adminId);
+               //Construct admin object
+               Admin admin = new Admin(adminName, adminId);
 
               //Add the admin to the hashmap
               staffService.addAdmin(admin);
-
-              boolean adminMenuActive = true;
               System.out.println("-------------------------------------");
-
-              while(adminMenuActive){
-                System.out.println("Welcome " + adminName + " what would you like to do?");
-                System.out.println("1 - Add a teacher");
-                System.out.println("2 - Add a course");
-                System.out.println("3 - Add a student");
-                System.out.println("4 - Add a student to a course");
-                System.out.println("5 - View Students Enrolled in Course");
-                System.out.println("0 - Exit");
-
-                int adminMenuChoice = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("-------------------------------------");
-
-                switch(adminMenuChoice){
-                    case 1:
-                        System.out.println("Add a teacher");
-                        validInput = false;
-                        int teacherId = -1;
-
-                        //We need to make sure the user enters a number for teacher id
-                        while(!validInput){
-                            System.out.print("Enter teacher Id: ");
-                            if(scanner.hasNextInt()){
-                                teacherId = scanner.nextInt();
-                                scanner.nextLine();
-                                validInput = true;
-                            }
-                            else{
-                                System.out.println("Please enter a non-demical number for teacher input");
-                                scanner.nextLine();
-                            }
-                        }
-
-                        System.out.print("Enter the teachers name: ");
-                        String teacherName = scanner.nextLine();
-
-                        System.out.print("Enter the teachers email: ");
-                        String teacherEmail = scanner.nextLine();
-
-                        //We create a teacher object and add the teacher to the staffService instance
-                        Teacher teacher = new Teacher(teacherId, teacherName, teacherEmail);
-                        staffService.addTeacher(teacher);
-                        System.out.println("-------------------------------------");
-                        break;
-                    case 2:
-                        System.out.println("Create a course");
-                        System.out.println("Please enter course ID");
-                        int courseId= scanner.nextInt();
-                        scanner.nextLine();
-
-                        System.out.println("Enter the course name");
-                        String courseName = scanner.nextLine();
-
-                        System.out.println("Enter course description");
-                        String courseDescription = scanner.nextLine();
-                        System.out.println("-------------------------------------");
-
-                        staffService.listAllTeachers();
-                        System.out.println("Select a teacher to assign (enter teacher ID): ");
-                        int selectedTeacherId = scanner.nextInt();
-                        scanner.nextLine();
-
-                        //Find the selected teacher
-                        Teacher selectedTeacher = staffService.findTeacherById(selectedTeacherId);
-
-                        //Create course object and add the teacher to it
-                        Course course = new Course(courseId, courseName, courseDescription, selectedTeacher);
-                        //Add course to CourseService
-                        courseService.addCourse(course);
-
-                        if(selectedTeacher != null){
-                            admin.addCourseToTeacher(selectedTeacher, course);
-                        }
-                        else{
-                            System.out.println("Teacher not found");
-                        }
-                        System.out.println("-------------------------------------");
-                        break;
-                    case 3:
-                        System.out.println("Add a student");
-                        validInput = false;
-                        int studentId = -1;
-
-                        while(!validInput){
-                            System.out.print("Enter student Id");
-                            if(scanner.hasNextInt()){
-                                studentId = scanner.nextInt();
-                                scanner.nextLine();
-                                validInput = true;
-                            }
-                            else{
-                                System.out.println("Please enter a non-decimal number for student Id");
-                                scanner.nextLine();
-                            }
-                        }
-
-                        System.out.print("Enter the students name: ");
-                        String studentName = scanner.nextLine();
-
-                        System.out.print("Enter the students email: ");
-                        String studentEmail = scanner.nextLine();
-
-                        Student student = new Student(studentName, studentId, studentEmail);
-                        studentDirectory.addStudent(student);
-                        System.out.println("-------------------------------------");
-                        break;
-
-                    case 4:
-                        System.out.println("Add a student to course");
-                        studentDirectory.listAllStudents();
-                        System.out.println("Select a student to assign (enter student ID): ");
-                        studentId = scanner.nextInt();
-                        scanner.nextLine();
-
-                        //Find the selected student
-                        Student selectedStudent = studentDirectory.getStudentById(studentId);
-
-                        //Select course that you would like to add student to
-                        System.out.println("Please select a course to add the student to");
-                        courseService.listCourses();
-                        System.out.println("-------------------------------------");
-                        System.out.println("Enter course Id");
-                        courseId = scanner.nextInt();
-                        scanner.nextLine();
-
-                        //Now retrieve the course object and add the student to it
-                        course = courseService.getCourseById(courseId);
-                        course.addStudent(selectedStudent);
-                        System.out.println("-------------------------------------");
-                        break;
-                    case 5:
-                        System.out.println("View students enrolled in course");
-                        courseService.listCourses();
-
-                        //Locate the course object and print the students
-                        System.out.println("Enter the course Id for the course you want to see");
-                        courseId = scanner.nextInt();
-                        scanner.nextLine();
-
-                        course = courseService.getCourseById(courseId);
-                        course.listStudentsInCourse();
-                        System.out.println("-------------------------------------");
-                        break;
-                    case 0:
-                        adminMenuActive = false;
-                        break;
-                }
-            }
+              
+              AdminMenu adminMenu = new AdminMenu(staffService, studentDirectory, courseService, admin);
+              adminMenu.displayMenu();
+              break;
         }
     }
 }
